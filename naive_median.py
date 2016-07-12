4a9686c6dd055d8b2224edee15bbe04fad4520d4
chu <chu@cs.nmsu.edu>
2016-07-12 11:52:51 -0600

    refactor group log_mean method, add factors for all group_mean

--- naive_median.py
+++ naive_median.py
@@ -13,71 +13,12 @@ import numpy as np
 import ConfigParser
 from sklearn.metrics import mean_squared_error as MSE
 
-### https://www.kaggle.com/paulorzp/grupo-bimbo-inventory-demand/mean-median-lb-0-48/code
-def group_median_predict_pid(df_train, df_test):
-    start_time = time.time()
-    all_median = df_train['Demanda_uni_equil'].median()
-
-    product_mean = df_train.groupby(['Producto_ID','Agencia_ID'])['log_Demanda_uni_equil'].mean()
-    product_mean = product_mean.apply(lambda x:np.exp(x)*0.5793)
-
-    product_agent_mean = df_train.groupby(['Producto_ID','Agencia_ID','Cliente_ID'])\
-            ['log_Demanda_uni_equil'].mean()
-    product_agent_mean = product_agent_mean.apply(lambda x:np.exp(x)-0.91)
-    print 'median calculating time=', time.time()-start_time
-
-    start_time = time.time()
-    df_test['Demanda_uni_equil'] = all_median
-    df_test['Demanda_uni_equil'] = df_test.apply(\
-            lambda x : predict_demand(x, product_agent_mean, product_mean, all_median), axis=1)
-    df_test.to_csv('output/product_agent_client_group_mean_'+ \
-            str(datetime.datetime.now().strftime('%Y-%m-%d-%H-%M'))+'.csv', \
-            columns=['id','Demanda_uni_equil'], index=False)
-    print 'predicting time=', time.time()-start_time
-
-def predict_demand_pname(x, product_agent_mean, product_mean, all_median):
-    # pid, aid, cid = x[['short_name','Agencia_ID','Cliente_ID']]
-    pid, aid, cid = x[0], x[1], x[2]
-    if (pid, aid, cid) in product_agent_mean.index:
-        return product_agent_mean[(pid, aid, cid)]
-    elif (pid, aid) in product_mean.index:
-        return product_mean[(pid, aid)]
-    else:
-        return all_median
-
-## https://www.kaggle.com/paulorzp/grupo-bimbo-inventory-demand/mean-median-lb-0-48/code
-## use product short name
-def group_median_predict_pname(df_train, df_test):
-    start_time = time.time()
-    all_median = df_train['Demanda_uni_equil'].median()
-
-    product_mean = df_train.groupby(['short_name','Agencia_ID'])['log_Demanda_uni_equil'].mean()
-    product_mean = product_mean.apply(lambda x:np.exp(x)*0.5793)
-
-    product_agent_mean = df_train.groupby(['short_name','Agencia_ID','Cliente_ID'])\
-            ['log_Demanda_uni_equil'].mean()
-    product_agent_mean = product_agent_mean.apply(lambda x:np.exp(x)-0.91)
-    print 'median calculating time=', time.time()-start_time
-
-    start_time = time.time()
-    df_test['Demanda_uni_equil'] = all_median
-
-    # pandas dataframe apply is slow for create series for each row
-    df_test['Demanda_uni_equil'] = np.apply_along_axis( \
-            (lambda x : predict_demand_pname(x, product_agent_mean, product_mean, all_median)),\
-            1, df_test[['short_name','Agencia_ID','Cliente_ID']].values)
-    df_test.to_csv('output/pname_agent_client_group_mean_'+ \
-            str(datetime.datetime.now().strftime('%Y-%m-%d-%H-%M'))+'.csv', \
-            columns=['id','Demanda_uni_equil'], index=False)
-    print 'predicting time=', time.time()-start_time
-
 def log_means_predict_demand(x, labels1_mean, labels2_mean, labels3_mean, all_mean, \
         labels1_idx, labels2_idx, labels3_idx, mult_factor, plus_factor):
     x_l1 = tuple(x[labels1_idx])
     x_l2 = tuple(x[labels2_idx])
     x_l3 = tuple(x[labels3_idx])
 
-    # print x_l1, ';', x_l2, ';', x_l3
     pred = 0
     if x_l1 in labels1_mean.index:
         pred = labels1_mean[x_l1]
@@ -89,6 +30,25 @@ def log_means_predict_demand(x, labels1_mean, labels2_mean, labels3_mean, all_me
         pred = all_mean
 
     return np.expm1(pred)*mult_factor+plus_factor
+
+def log_means_predict_demand(x, labels1_mean, labels2_mean, labels3_mean, all_mean, \
+        labels1_idx, labels2_idx, labels3_idx, mult_factor1, plus_factor1, \
+        mult_factor2,plus_factor2, mult_factor3,plus_factor3, mult_factor4, plus_factor4):
+    x_l1 = tuple(x[labels1_idx])
+    x_l2 = tuple(x[labels2_idx])
+    x_l3 = tuple(x[labels3_idx])
+
+    pred = 0
+    if x_l1 in labels1_mean.index:
+        pred = labels1_mean[x_l1]*mult_factor1+plus_factor1
+    elif x_l2 in labels2_mean.index:
+        pred = labels2_mean[x_l2]*mult_factor2+plus_factor2
+    elif x_l3 in labels3_mean.index:
+        pred = labels3_mean[x_l3]*mult_factor3+plus_factor3
+    else:
+        pred = all_mean*mult_factor4+plus_factor4
+
+    return pred
 
 ## https://www.kaggle.com/apapiu/grupo-bimbo-inventory-demand/log-means/code
 def group_log_means_predict_pid(df_train, df_test, config_path, config_name, vali):
@@ -104,9 +64,30 @@ def group_log_means_predict_pid(df_train, df_test, config_path, config_name, val
     labels3_idx = [labels.index(i) for i in labels3]
     # print labels1_idx, ';', labels2_idx, ';', labels3_idx
 
-    mult_factor = config.getfloat(config_name, 'mult_factor')
-    plus_factor = config.getfloat(config_name, 'plus_factor')
-    print 'mult_factor=', mult_factor, ' plus_factor=', plus_factor
+    mult_factor1 = mult_factor2 = mult_factor3 = mult_factor4 = 1
+    plus_factor1 = plus_factor2 = plus_factor3 = plus_factor4 = 0
+    if 'level' in config_name:
+        mult_factor1, mult_factor2, mult_factor3, mult_factor4 = \
+                [config.getfloat(config_name, 'mult_factor'+str(i)) for i in range(1,5)]
+                # config.getfloat(config_name, 'mult_factor1'), \
+                # config.getfloat(config_name, 'mult_factor2'), \
+                # config.getfloat(config_name, 'mult_factor3'), \
+                # config.getfloat(config_name, 'mult_factor4')
+        plus_factor1, plus_factor2, plus_factor3, plus_factor4 = \
+                [config.getfloat(config_name, 'plus_factor'+str(i)) for i in range(1,5)]
+                # config.getfloat(config_name, 'plus_factor1'), \
+                # config.getfloat(config_name, 'plus_factor2'), \
+                # config.getfloat(config_name, 'plus_factor3'), \
+                # config.getfloat(config_name, 'plus_factor4')
+    else:
+        mult_factor1 = mult_factor2 = mult_factor3 = mult_factor4 = \
+                config.getfloat(config_name, 'mult_factor')
+        plus_factor1 = plus_factor2 = plus_factor3 = plus_factor4 = \
+                config.getfloat(config_name, 'plus_factor')
+    print 'mult_factor1=', mult_factor1, ' plus_factor1=', plus_factor1
+    print 'mult_factor2=', mult_factor2, ' plus_factor2=', plus_factor2
+    print 'mult_factor3=', mult_factor3, ' plus_factor3=', plus_factor3
+    print 'mult_factor4=', mult_factor4, ' plus_factor4=', plus_factor4
 
     start_time = time.time()
     all_mean = df_train['log_Demanda_uni_equil'].mean()
@@ -120,7 +101,8 @@ def group_log_means_predict_pid(df_train, df_test, config_path, config_name, val
     if not vali:
         df_test['Demanda_uni_equil']=np.apply_along_axis((lambda x:log_means_predict_demand(x, \
             labels1_mean, labels2_mean, labels3_mean, all_mean, \
-            labels1_idx, labels2_idx, labels3_idx, mult_factor, plus_factor)), \
+            labels1_idx, labels2_idx, labels3_idx, mult_factor1,plus_factor1, \
+            mult_factor2,plus_factor2, mult_factor3,plus_factor3, mult_factor4,plus_factor4)), \
             1, df_test[labels].values)
         df_test.to_csv('output/'+'_'.join(labels)+'_'+config_name+'_group_log_mean_'+ \
                 str(datetime.datetime.now().strftime('%Y-%m-%d-%H-%M'))+'.csv', \
@@ -129,7 +111,8 @@ def group_log_means_predict_pid(df_train, df_test, config_path, config_name, val
         # global pred_demand, true_demand
         pred_demand = np.apply_along_axis((lambda x:log_means_predict_demand(x, \
             labels1_mean, labels2_mean, labels3_mean, all_mean, \
-            labels1_idx, labels2_idx, labels3_idx, mult_factor, plus_factor)), \
+            labels1_idx, labels2_idx, labels3_idx, mult_factor1,plus_factor1, \
+            mult_factor2,plus_factor2, mult_factor3,plus_factor3, mult_factor4,plus_factor4)), \
             1, df_test[labels].values)
         true_demand = df_test['Demanda_uni_equil'].values
         RMSLE = np.sqrt(MSE(np.log1p(pred_demand), np.log1p(true_demand)))
@@ -154,6 +137,8 @@ if __name__ == '__main__':
         df_test = df_train[(df_train['Semana']>time_quantile) & \
             (df_train['Cliente_ID'].isin(pandas.unique(df_test['Cliente_ID'])))]
         df_train = df_train[df_train['Semana']<=time_quantile]
+    # else: # test now
+    #     df_train = df_train[(df_train['Cliente_ID'].isin(pandas.unique(df_test['Cliente_ID'])))]
 
     for config_name in config_name_list:
         if not vali:
